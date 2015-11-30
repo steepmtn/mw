f7aa109a61bc50192710339b4073e5e76190d895
Joshua Chen <joshuachen02@gmail.com>
2015-11-30 11:21:27 -0800

    Add member variables and seters/geters.

--- src/main/java/util/Product.java
+++ src/main/java/util/Product.java
@@ -1,9 +1,34 @@
 package util;
 
 public class Product {
+	private int id;
+	private String name;
+	
   
 	@Override
 	public String toString(){
 		return String.valueOf(this.hashCode());
 	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
